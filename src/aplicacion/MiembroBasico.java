e70ff9c8affb63e115a37d5fa803cdc43a6739cf
Iago Feijóo Rey <iago.feijoo.rey@gmail.com>
2024-05-02 01:27:23 +0200

    Add files via upload

---
+++ src/aplicacion/MiembroBasico.java
@@ -0,0 +1,29 @@
+package aplicacion;
+
+/**
+ *
+ * @author basesdatos
+ */
+
+/*
+public class MiembroBasico extends Acolito {
+    private JefeDivision jefe;
+
+   public MiembroBasico(String idUsuario, String clave, String nombre, String direccion, String email, int influencia, JefeDivision jefe){
+       super(idUsuario, clave, nombre, direccion, email, influencia, (tipoAcolito) Normal);
+        this.jefe = jefe;
+   }
+
+   //Getter
+
+    public JefeDivision getJefe() {
+        return jefe;
+    }
+
+    //Setter
+    public void setJefe (JefeDivision jefe) {
+        this.jefe = jefe;
+    }
+
+}
+*/
---
+++ src/aplicacion/TipoAcolito.java
@@ -0,0 +1,33 @@
+package aplicacion;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author basesdatos
+ */
+public enum TipoAcolito {
+    //Administrador,
+    Cabecilla,
+    JefeDivision,
+    Gestor,
+    LiderEspiritual,
+    Normal;
+
+
+    public static TipoAcolito stringToTipoAcolito(String tu){
+        switch (tu){
+            case ("Cabecilla"):
+                return Cabecilla;
+            case ("Gestor"):
+                return Gestor;
+            default:
+                return Normal;
+        }
+    }
+}
